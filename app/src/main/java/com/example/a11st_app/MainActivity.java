709d4317a3a4a0cf492f280c45d822ff9e6329e9
y26g <rkwhr0215@naver.com>
2022-05-24 16:15:35 +0900

    영화정보의수 추가

--- app/src/main/java/com/example/a11st_app/MainActivity.java
+++ app/src/main/java/com/example/a11st_app/MainActivity.java
@@ -15,6 +15,7 @@ import com.android.volley.Response;
 import com.android.volley.VolleyError;
 import com.android.volley.toolbox.StringRequest;
 import com.android.volley.toolbox.Volley;
+import com.google.gson.Gson;
 
 import java.util.HashMap;
 import java.util.Map;
@@ -55,6 +56,7 @@ public class MainActivity extends AppCompatActivity {
                     @Override
                     public void onResponse(String response) {
                         println("응답 -> " + response);
+                        processResponse(response);
                     }
                 },
                 new Response.ErrorListener() {
@@ -77,7 +79,15 @@ public class MainActivity extends AppCompatActivity {
         println("요청 보냄.");
     }
 
-    public void println(String data) {
+    public void println(String data)
+    {
         responseText.append(data + "\n");
+    }
+
+    public void processResponse(String response)
+    {
+        Gson gson = new Gson();
+        MovieList movieList = gson.fromJson(response, MovieList.class);
+        println("영화 정보의 수 : " + movieList.boxOfficeResult.dailyBoxOfficeList.size());
     }
 }
